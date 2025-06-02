761397a98b607def18d90d247afe0ceedbb2c8e0
sadin07 <sadinthapa@gmail.com>
2025-06-02 10:08:02 +0545

    env setup

--- task8/settings.py
+++ task8/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', cast=bool)
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cnpuysa!76f@t)fh$hsxnhg1wpcl@*#j!m$k%x=l_)_s7+289d'
+ 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+ 
+
 
 ALLOWED_HOSTS = []
 
